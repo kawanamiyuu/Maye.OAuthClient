603d1b44d3aa42b6d40ba7bc435bb198a72ac073
kawanamiyuu <i.am.really.you@gmail.com>
2015-02-03 13:09:53 +0900

    add comment

--- src/OAuth1ClientInterface.php
+++ src/OAuth1ClientInterface.php
@@ -7,6 +7,8 @@ use OAuth\OAuth1\Token\TokenInterface;
 interface OAuth1ClientInterface extends OAuthClientInterface
 {
     /**
+     * Retrieve an OAuth1 access token
+     *
      * @param string $token    OAuth Token
      * @param string $verifier OAuth Verifier
      *
