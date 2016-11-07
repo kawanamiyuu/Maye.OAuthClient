1cff6a1677552986a7e136fdf2c2952d8131f002
kawanamiyuu <i.am.really.you@gmail.com>
2016-11-07 22:53:28 +0900

    update qa mata files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-require "functions.php";
+require 'functions.php';
 
 $_SERVER['REQUEST_URI'] = 'http://example.com/test?key=value';
 $_SESSION = [];
